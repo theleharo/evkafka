a2bad580a18a90c135b5ae1e5c3d4c456d6e0a72
theleharo <theleharo@gmail.com>
2023-11-06 15:05:29 +0300

    fix opt fields in asyncapi model

--- evkafka/asyncapi/models.py
+++ evkafka/asyncapi/models.py
@@ -4,16 +4,16 @@ from pydantic import AnyUrl, BaseModel, EmailStr, Field, StringConstraints
 
 
 class Contact(BaseModel):
-    name: str | None
-    url: AnyUrl | None
-    email: EmailStr | None
+    name: str | None = None
+    url: AnyUrl | None = None
+    email: EmailStr | None = None
 
     model_config = {"extra": "allow"}
 
 
 class License(BaseModel):
     name: str
-    url: AnyUrl | None
+    url: AnyUrl | None = None
 
     model_config = {"extra": "allow"}
 
@@ -21,25 +21,25 @@ class License(BaseModel):
 class Info(BaseModel):
     title: str
     version: str
-    description: str
-    termsOfService: AnyUrl | None
-    contact: Contact | None
-    license: License | None
+    description: str | None = None
+    termsOfService: AnyUrl | None = None
+    contact: Contact | None = None
+    license: License | None = None
 
     model_config = {"extra": "allow"}
 
 
 class ServerVariable(BaseModel):
-    enum: list[str] | None
-    default: str | None
-    description: str | None
-    examples: list[str] | None
+    enum: list[str] | None = None
+    default: str | None = None
+    description: str | None = None
+    examples: list[str] | None = None
 
     model_config = {"extra": "allow"}
 
 
 class ExternalDocumentation(BaseModel):
-    description: str | None
+    description: str | None = None
     url: AnyUrl
 
     model_config = {"extra": "allow"}
@@ -47,21 +47,21 @@ class ExternalDocumentation(BaseModel):
 
 class Tag(BaseModel):
     name: str
-    description: str | None
-    externalDocs: ExternalDocumentation | None
+    description: str | None = None
+    externalDocs: ExternalDocumentation | None = None
 
     model_config = {"extra": "allow"}
 
 
 class KafkaServerBinding(BaseModel):
-    schemaRegistryUrl: AnyUrl | None
-    schemaRegistryVendor: str | None
+    schemaRegistryUrl: AnyUrl | None = None
+    schemaRegistryVendor: str | None = None
     bindingVersion: str | None = "latest"
 
 
 class ServerBindings(BaseModel):
     # only https://github.com/asyncapi/bindings/tree/master/kafka#server
-    kafka: KafkaServerBinding | None
+    kafka: KafkaServerBinding | None = None
 
     model_config = {"extra": "allow"}
 
@@ -73,13 +73,13 @@ class Reference(BaseModel):
 class Server(BaseModel):
     url: AnyUrl
     protocol: str
-    protocolVersion: str | None
-    description: str | None
-    variables: dict[str, ServerVariable | Reference] | None
+    protocolVersion: str | None = None
+    description: str | None = None
+    variables: dict[str, ServerVariable | Reference] | None = None
     # TBD SecurityRequirement https://www.asyncapi.com/docs/reference/specification/v2.6.0#securityRequirementObject
-    security: list[dict[str, list[str]]] | None
-    tags: list[Tag] | None
-    bindings: ServerBindings | Reference | None
+    security: list[dict[str, list[str]]] | None = None
+    tags: list[Tag] | None = None
+    bindings: ServerBindings | Reference | None = None
 
     model_config = {"extra": "allow"}
 
@@ -144,7 +144,9 @@ class Schema(BaseModel):
     properties: dict[str, "SchemaOrBool"] | None = None
     patternProperties: dict[str, "SchemaOrBool"] | None = None
     additionalProperties: Union["SchemaOrBool", None] = None
-    dependencies: dict[str, set[str] | "SchemaOrBool"] | None = None  # 2019-09: split
+    dependencies: dict[
+        str, Union[set[str], "SchemaOrBool"]
+    ] | None = None  # 2019-09: split
     propertyNames: Union["SchemaOrBool", None] = None
 
     if_: Union["SchemaOrBool", None] = Field(default=None, alias="if")
@@ -222,32 +224,32 @@ SchemaOrBool = Schema | bool
 
 
 class KafkaOperationBinding(BaseModel):
-    groupId: SchemaOrBool | Reference | None
-    clientId: SchemaOrBool | Reference | None
+    groupId: SchemaOrBool | Reference | None = None
+    clientId: SchemaOrBool | Reference | None = None
     bindingVersion: str | None = "latest"
 
 
 class OperationBindings(BaseModel):
     # only https://github.com/asyncapi/bindings/blob/master/kafka/README.md#channel
-    kafka: KafkaOperationBinding | None
+    kafka: KafkaOperationBinding | None = None
 
     model_config = {"extra": "allow"}
 
 
 class OperationTrait(BaseModel):
-    operationId: str | None
-    summary: str | None
-    description: str | None
-    security: list[dict[str, list[str]]] | None
-    tags: list[Tag] | None
-    externalDocs: ExternalDocumentation | None
-    bindings: OperationBindings | Reference | None
+    operationId: str | None = None
+    summary: str | None = None
+    description: str | None = None
+    security: list[dict[str, list[str]]] | None = None
+    tags: list[Tag] | None = None
+    externalDocs: ExternalDocumentation | None = None
+    bindings: OperationBindings | Reference | None = None
 
     model_config = {"extra": "allow"}
 
 
 class CorrelationId(BaseModel):
-    description: str | None
+    description: str | None = None
     location: str
 
     model_config = {"extra": "allow"}
@@ -255,58 +257,58 @@ class CorrelationId(BaseModel):
 
 class KafkaMesssageBinding(BaseModel):
     key: SchemaOrBool | Reference
-    schemaIdLocation: str | None
-    schemaIdPayloadEncoding: str | None
-    schemaLookupStrategy: str | None
+    schemaIdLocation: str | None = None
+    schemaIdPayloadEncoding: str | None = None
+    schemaLookupStrategy: str | None = None
 
 
 class MessageBindings(BaseModel):
-    kafka: KafkaMesssageBinding | None
+    kafka: KafkaMesssageBinding | None = None
 
     model_config = {"extra": "allow"}
 
 
 class MessageExample(BaseModel):
-    headers: dict[str, Any] | None
+    headers: dict[str, Any] | None = None
     payload: Any
-    name: str | None
-    summary: str | None
+    name: str | None = None
+    summary: str | None = None
 
     model_config = {"extra": "allow"}
 
 
 class MessageTrait(BaseModel):
-    messageId: str | None
-    headers: SchemaOrBool | Reference | None
-    correlationId: CorrelationId | Reference | None
-    schemaFormat: str | None
-    contentType: str | None
-    name: str | None
-    title: str | None
-    summary: str | None
-    description: str | None
-    tags: list[Tag] | None
-    externalDocs: ExternalDocumentation | None
-    bindings: MessageBindings | Reference | None
-    examples: list[MessageExample] | None
+    messageId: str | None = None
+    headers: SchemaOrBool | Reference | None = None
+    correlationId: CorrelationId | Reference | None = None
+    schemaFormat: str | None = None
+    contentType: str | None = None
+    name: str | None = None
+    title: str | None = None
+    summary: str | None = None
+    description: str | None = None
+    tags: list[Tag] | None = None
+    externalDocs: ExternalDocumentation | None = None
+    bindings: MessageBindings | Reference | None = None
+    examples: list[MessageExample] | None = None
 
 
 class Message(BaseModel):
-    messageId: str | None
-    headers: SchemaOrBool | Reference | None
+    messageId: str | None = None
+    headers: SchemaOrBool | Reference | None = None
     payload: Any
-    correlationId: CorrelationId | Reference | None
-    schemaFormat: str | None
-    contentType: str | None
-    name: str | None
-    title: str | None
-    summary: str | None
-    description: str | None
-    tags: list[Tag] | None
-    externalDocs: ExternalDocumentation | None
-    bindings: MessageBindings | Reference | None
-    examples: list[MessageExample] | None
-    traits: list[MessageTrait | Reference] | None
+    correlationId: CorrelationId | Reference | None = None
+    schemaFormat: str | None = None
+    contentType: str | None = None
+    name: str | None = None
+    title: str | None = None
+    summary: str | None = None
+    description: str | None = None
+    tags: list[Tag] | None = None
+    externalDocs: ExternalDocumentation | None = None
+    bindings: MessageBindings | Reference | None = None
+    examples: list[MessageExample] | None = None
+    traits: list[MessageTrait | Reference] | None = None
 
     model_config = {"extra": "allow"}
 
@@ -316,51 +318,51 @@ class MultipleMessages(BaseModel):
 
 
 class Operation(BaseModel):
-    operationId: str | None
-    summary: str | None
-    description: str | None
-    security: list[dict[str, list[str]]] | None
-    tags: list[Tag] | None
-    externalDocs: ExternalDocumentation | None
-    bindings: OperationBindings | Reference | None
-    traits: list[OperationTrait | Reference] | None
-    message: Message | Reference | MultipleMessages | None
+    operationId: str | None = None
+    summary: str | None = None
+    description: str | None = None
+    security: list[dict[str, list[str]]] | None = None
+    tags: list[Tag] | None = None
+    externalDocs: ExternalDocumentation | None = None
+    bindings: OperationBindings | Reference | None = None
+    traits: list[OperationTrait | Reference] | None = None
+    message: Message | Reference | MultipleMessages | None = None
 
     model_config = {"extra": "allow"}
 
 
 class Parameter(BaseModel):
-    description: str | None
+    description: str | None = None
     schema_: SchemaOrBool | Reference | None = Field(default=None, alias="schema")
-    location: str | None
+    location: str | None = None
 
 
 class KafkaChannelBinding(BaseModel):
-    topic: str | None
-    partitions: int | None
-    replicas: int | None
-    topicConfiguration: TopicConfiguration | None
+    topic: str | None = None
+    partitions: int | None = None
+    replicas: int | None = None
+    topicConfiguration: TopicConfiguration | None = None
     bindingVersion: str | None = "latest"
 
 
 class ChannelBindings(BaseModel):
     # only https://github.com/asyncapi/bindings/blob/master/kafka/README.md#channel
-    kafka: KafkaChannelBinding | None
+    kafka: KafkaChannelBinding | None = None
 
     model_config = {"extra": "allow"}
 
 
 class ChannelItem(BaseModel):
-    ref: str = Field(alias="$ref")
-    description: str | None
-    servers: list[str] | None
-    subscribe: Operation | None
-    publish: Operation | None
+    ref: str = Field(default=None, alias="$ref")
+    description: str | None = None
+    servers: list[str] | None = None
+    subscribe: Operation | None = None
+    publish: Operation | None = None
     parameters: dict[
         Annotated[str, StringConstraints(pattern=r"^[A-Za-z0-9_\-]+$")],
         Parameter | Reference,
-    ]
-    bindings: ChannelBindings | Reference | None
+    ] | None = None
+    bindings: ChannelBindings | Reference | None = None
 
     model_config = {"extra": "allow"}
 
@@ -374,8 +376,8 @@ class SecuritySchemas(BaseModel):
         "scramSha256",
         "scramSha512",
         "gssapi",
-    ] | None
-    description: str | None
+    ] | None = None
+    description: str | None = None
 
     model_config = {"extra": "allow"}
 
@@ -401,17 +403,17 @@ class Components(BaseModel):
 
 class AsyncAPI(BaseModel):
     asyncapi: str = "2.6.0"
-    id: str | None
+    id: str | None = None
     info: Info
     servers: dict[
         Annotated[str, StringConstraints(pattern=r"^[A-Za-z0-9_\-]+$")],
         Server | Reference,
     ]
-    defaultContentType: str | None
+    defaultContentType: str | None = None
     channels: dict[str, ChannelItem]
-    components: Components | None
-    tags: list[Tag]
-    externalDocs: ExternalDocumentation | None
+    components: Components | None = None
+    tags: list[Tag] | None = None
+    externalDocs: ExternalDocumentation | None = None
 
 
 Schema.model_rebuild()
